9e455490a649c4d6bf8f4226ef5c78c746c4cce3
Timoh97 <timohmugendi@gmail.com>
2022-06-14 16:13:30 +0300

    consumed API

--- application/views.py
+++ application/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
 
+
+
+import http.client
+import json
+from .api import api_key
+conn = http.client.HTTPSConnection("vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com")
+
+headers = {
+    'X-RapidAPI-Key': api_key,
+    'X-RapidAPI-Host': "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com"
+    }
+
+conn.request("GET", "/api/npm-covid-data/", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+name=data.decode("utf-8")
+conv = json.loads(name)
+# for country in conv:
+#     print(country)
 # Create your views here.
-def index(request):
+
     
-    return render(request,'main/index.html')
+def index(request):
+     covid=conv
+
+     covid={'covid':covid}
+
+     return render(request,'main/index.html',covid)
